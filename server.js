c8c815cfaa5261fb242240971864a211f3422173
nagema <nagema@angelica.local>
2021-12-07 00:54:43 +0100

    change server port to 3001

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(443, () => {
+server.listen(3001, () => {
   console.log('JSON Server is running')
 })
